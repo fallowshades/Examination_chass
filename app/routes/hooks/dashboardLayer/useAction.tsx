b38fe5bd93c377d3f4c2fc94bdba8ecc249ebfda
Erik Jonsson <fallowshades@gmail.com>
2025-05-29 08:49:44 +0200

    Update useAction.tsx

--- app/routes/hooks/dashboardLayer/useAction.tsx
+++ app/routes/hooks/dashboardLayer/useAction.tsx
@@ -3,6 +3,8 @@
 //loader need return session, there is a action setup.
 ;```tsx
 return json(
+  { ...data, error },
+  // you should commit the session to remove the flash key
   { headers: { 'Set-Cookie': await commitSession(session) } }
 )
 ```
